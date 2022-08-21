8735b8a17fdd7f94ace3016c773ada019b48acaa
Ekaterina Mitiashkina <algrince@gmail.com>
2022-08-21 23:28:12 +0200

    linter fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,15 +8,14 @@ DESCRIPTION = "Compares two configuration files and shows the difference"
 
 
 parser = argparse.ArgumentParser(description=DESCRIPTION)
-parser.add_argument('first_file', 
-        type=str, metavar='first_file')
-parser.add_argument('second_file', 
-        type=str, metavar='second_file')
-parser.add_argument(
-        '-f', '--format',
-        metavar='FORMAT', 
-        default='json', 
-        help='set format of output')
+parser.add_argument('first_file',
+                    type=str, metavar='first_file')
+parser.add_argument('second_file',
+                    type=str, metavar='second_file')
+parser.add_argument('-f', '--format',
+                    metavar='FORMAT',
+                    default='json',
+                    help='set format of output')
 
 
 def main():
