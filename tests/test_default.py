d855473c84511b9fe78d3914d831f1c329785843
Ekaterina Mitiashkina <algrince@gmail.com>
2022-08-22 22:47:07 +0200

    github actions fix

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,9 +5,9 @@ from gendiff.gen_diff import generate_diff
 
 
 def test_format():
-    with open('fixtures/expected.txt','r') as fixture:
+    with open('tests/fixtures/expected.txt','r') as fixture:
         expected = fixture.read()
     assert expected == generate_diff(
-            'fixtures/file1.json', 
-            'fixtures/file2.json'
+            'tests/fixtures/file1.json', 
+            'tests/fixtures/file2.json'
     )
