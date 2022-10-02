a98a047a7fb3fbb22523e39395c6af2c62ed161e
Ekaterina Mitiashkina <algrince@gmail.com>
2022-10-02 12:29:51 +0200

    plain format update

--- gendiff/formats/plain.py
+++ gendiff/formats/plain.py
@@ -12,42 +12,57 @@ statuses = {
 
 def formate_plain(data, nest_lvl=0):
     sorted_data = sort_data(data)
-    plain_data = decode_plain(sorted_data, nest_lvl=0)
-    data_string = ''
-    for line in plain_data:
-        key, value1, value2, status = line
-        added_status = statuses[status]
-        data_string += added_status.format(
-            key=key, value1=value1, value2=value2)
-    return data_string
-
-
-
-
-def decode_plain(sorted_data, nest_lvl=0):
-    plain_data = []
     for line in sorted_data:
-        print(line)
-        key, value, state, nest, update = line
-        value2 = None
-        if state == 'equal' or update == 'updated':
+        if line[3] == 'nested':
+            level = nest_lvl + 1
+            line[1] = formate_plain(line[1], nest_lvl=level)
+    string_data = make_plain(sorted_data, nest_lvl)
+    return string_data
+
+
+def make_plain(sorted_data, nest_lvl=0):
+    string_diff = ''
+    for line in sorted_data:
+        print('sorted line', line)
+        if line[2] == 'equal' or line[4] == 'updated':
             continue
-        elif nest == 'nested':
-            if isinstance(update, dict):
-                value2 = update.get('updated')
-                value, value2 = transform_complex(value, value2)
-            else:
-                lvl = nest_lvl + 1
-                nested_line = decode_plain(line, nest_lvl=lvl)
-                nested_key = nested_line[0]
-                key += nested_key
-        plain_data.append([key, value, value2, state])
-    return plain_data
+        else:
+            plain_line = make_plain_line(line, nest_lvl=0)
+        state = line[2]
+        added_status = statuses[state]
+        if state == 'added':
+            string_diff += added_status.format(
+                key=line[0], 
+                value1=line[1]
+            )
+        elif state == 'removed':
+            string_diff += added_status.format(
+                key=line[0]
+            )
+        elif state == 'updated':
+            string_diff += added_status.format(
+                key=line[0],
+                value1=line[1],
+                value2=line[2]
+            )
+    return string_diff
 
 
-def transform_compex(var1, var2):
+def transform_complex(var1, var2):
     if isinstance(var1, dict):
         var1 = '[complex value]'
     elif isinstance(var2, dict):
         var2 = '[complex value]'
     return var1, var2
+
+
+def make_plain_line(line, nest_lvl=0):
+    print(line)
+    key, value, state, nest, update = line
+    if isinstance(update, dict):
+        value2 = update.get('updated')
+        state = 'updated'
+    else:
+        value2 = None
+    value, value2 = transform_complex(value, value2)
+    return [key, value, value2, state]
