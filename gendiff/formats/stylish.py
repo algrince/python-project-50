4f1979ff38e1a793140e20cff2a960fe2b5eb856
Ekaterina Mitiashkina <algrince@gmail.com>
2023-01-29 21:30:43 +0100

    rewrite stylish formatter

--- gendiff/formats/stylish.py
+++ gendiff/formats/stylish.py
@@ -2,76 +2,79 @@
 
 from gendiff.decoder import decode
 
-signs = {'unchanged': '  ', 'nested': '  ', 'removed': '- ', 'added': '+ '}
+statuses = {
+    'unchanged': '  {key}: {value}\n',
+    'removed': '- {key}: {value}\n',
+    'added': '+ {key}: {value}\n',
+    'nested': '  {key}: {value}\n',
+    'changed': ''
+}
 
 
-def format_diff_to_stylish(data, nest_lvl=0):
-    '''Fomates raw diff in stylish format'''
-    for node in data:
-        info = data[node]
-        node_type, values = info
-        if node_type == 'nested':
-            level = nest_lvl + 1
-            values = format_diff_to_stylish(values, nest_lvl=level)
-            data[node] = [node_type, values]
-    string_data = make_stylish_represent(data, nest_lvl)
+def format_diff_to_stylish(data):
+    string_data = make_stylish_represent(data)
     return string_data
 
 
-def make_stylish_dict(var, nest_lvl):
-    '''Formates dictionary in stylish representation'''
-    space_count = trans_lvl_to_spaces(nest_lvl)
-    count = nest_lvl
-    string_diff = '{\n'
-    if isinstance(var, dict):
-        for node in var:
-            if isinstance(var[node], dict):
-                var[node] = make_stylish_dict(var[node], nest_lvl + 1)
-            line = {node: ['unchanged', var[node]]}
-            string_line = make_line_from_node(line, nest_lvl=count + 1)
-            string_diff = "".join([string_diff, string_line])
-        ending_space = ' ' * (space_count + 2) + '}'
-        string_diff = "".join([string_diff, ending_space])
-        return string_diff
-    return var
-
-
 def make_stylish_represent(data, nest_lvl=0):
-    '''Formates data as stylish and fills it with lined diff'''
-    space_count = trans_lvl_to_spaces(nest_lvl)
-    string_diff = '{\n'
+    string_data = '{\n'
     for node in data:
-        line = {node: data[node]}
-        string_line = make_line_from_node(line, nest_lvl=nest_lvl)
-        string_diff = "".join([string_diff, string_line])
-    ending_space = ' ' * (space_count - 2) + '}'
-    string_diff = "".join([string_diff, ending_space])
-    return string_diff
+        node_type, values = data[node]
+        added_status = statuses[node_type]
+        spaces_before = trans_lvl_to_spaces(nest_lvl) * ' '
+        spaces_after = (trans_lvl_to_spaces(nest_lvl) - 2) * ' '
+        if isinstance(values, tuple):
+            val1, val2 = values
+            added_status1 = statuses['removed']
+            added_status2 = statuses['added']
+            string_line = ''.join([
+                spaces_before,
+                added_status1.format(
+                    key=node,
+                    value=style_value(val1, nest_lvl),
+                ),
+                spaces_before,
+                added_status2.format(
+                    key=node,
+                    value=style_value(val2, nest_lvl),
+                )
+            ])
+
+        else:
+            if node_type == 'nested':
+                values = make_stylish_represent(values, nest_lvl=nest_lvl + 1)
+            string_line = ''.join([
+                spaces_before,
+                added_status.format(
+                    key=node,
+                    value=style_value(values, nest_lvl)
+                )
+            ])
+        string_data = ''.join([string_data, string_line])
+    string_data = ''.join([string_data, spaces_after, '}'])
+    return string_data
 
 
-def make_line_from_node(node, formatter=' ', nest_lvl=0):
-    '''Formates line for default presentation'''
-    space_count = trans_lvl_to_spaces(nest_lvl)
-    key = list(node.keys())[0]
-    node_type, values = node[key]
-    count = nest_lvl
-    if node_type == 'changed':
-        value1 = make_stylish_dict(values[0], count)
-        value2 = make_stylish_dict(values[1], count)
-        string_line = make_line_from_node(
-            {key: ['removed', value1]}, nest_lvl=count
-        ) + make_line_from_node(
-            {key: ['added', value2]}, nest_lvl=count
-        )
+def style_value(value, nest_lvl):
+    if isinstance(value, dict):
+        new_nest_lvl = nest_lvl + 1
+        spaces_before = trans_lvl_to_spaces(new_nest_lvl) * ' '
+        spaces_after = (trans_lvl_to_spaces(new_nest_lvl) - 2) * ' '
+        added_status = statuses['unchanged']
+        string_data = '{\n'
+        for node in value:
+            string_line = ''.join([
+                spaces_before,
+                added_status.format(
+                    key=node,
+                    value=style_value(value[node], nest_lvl=new_nest_lvl)
+                )
+            ])
+            string_data = ''.join([string_data, string_line])
+        string_data = ''.join([string_data, spaces_after, '}'])
+        return string_data
     else:
-        value = make_stylish_dict(values, count)
-        new_string = '{}{}{}: {}\n'
-        string_line = new_string.format(
-            formatter * space_count,
-            signs[node_type],
-            decode(key),
-            decode(value))
-    return string_line
+        return decode(value)
 
 
 def trans_lvl_to_spaces(nest_lvl):
--- tests/test_stylish.py
+++ tests/test_stylish.py
@@ -2,13 +2,13 @@
 
 
 from gendiff.formats.stylish import (
-    make_stylish_dict, make_line_from_node, make_stylish_represent
+    make_stylish_represent, style_value
 )
 from gendiff.formats.plain import sort_data
 from gendiff.decoder import decode
 
 
-CHANGED = {'change_key': ['changed', ['value1', 'value2']]}
+CHANGED = {'change_key': ['changed', ('value1', 'value2')]}
 ADDED = {'add_key': ['added', 'add_val']}
 REMOVED = {'remove_key': ['removed', 'rmv_val']}
 UNCHANGED = {'key': ['unchanged', 'value']}
@@ -22,17 +22,6 @@ def test_decode():
     assert '12' == decode(12)
 
 
-def test_make_stylish_dict():
-    nested_var = {'key5': 'var5'}
-    simple_var = 'var6'
-
-    exp_nested_var = '{\n        key5: var5\n    }'
-    exp_simple_var = simple_var
-
-    assert exp_simple_var == make_stylish_dict(simple_var, 1)
-    assert exp_nested_var == make_stylish_dict(nested_var, 0)
-
-
 def test_make_make_stylish_represent():
     data = {**CHANGED, **ADDED, **REMOVED, **UNCHANGED}
     sorted_data = sort_data(data)
@@ -41,20 +30,11 @@ def test_make_make_stylish_represent():
     assert expected_data == make_stylish_represent(sorted_data)
 
 
-def test_make_line():
-    nested = {'nested_key': ['nested', {'key2': 'value'}]}
-    formated = {'key': ['unchanged', 'value']}
+def test_style_value():
+    value = {'key': 'value'}
+    nested_value = {'key': {'another key': 42}}
+    expected_for_val = '{\n        key: value\n    }'
+    expected_nested_val = '{\n        key: {\n            another key: 42\n        }\n    }'
 
-    exp_changed = '  - change_key: value1\n  + change_key: value2\n'
-    exp_added = '  + add_key: add_val\n'
-    exp_removed = '  - remove_key: rmv_val\n'
-    exp_unchanged = '    key: value\n'
-    exp_nested = '    nested_key: {\n        key2: value\n    }\n'
-    exp_formated = '111111  key: value\n'
-
-    assert exp_changed == make_line_from_node(CHANGED)
-    assert exp_added == make_line_from_node(ADDED)
-    assert exp_removed == make_line_from_node(REMOVED)
-    assert exp_unchanged == make_line_from_node(UNCHANGED)
-    assert exp_nested == make_line_from_node(nested)
-    assert exp_formated == make_line_from_node(formated, formatter='1', nest_lvl=1)
+    assert expected_for_val == style_value(value, nest_lvl=0)
+    assert expected_nested_val == style_value(nested_value, nest_lvl=0)
